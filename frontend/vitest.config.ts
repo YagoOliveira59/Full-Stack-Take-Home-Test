e410ebd45574ede854087379bef80f0ba9bcf14d
Yago Oliveira <ygocontato@gmail.com>
2024-08-05 02:56:52 -0300

    Add .env file to test

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 import path from 'path';
+
+dotenv.config();
 
 export default defineConfig({
     plugins: [react()],
@@ -12,6 +15,7 @@ export default defineConfig({
     test: {
         environment: 'jsdom',
         globals: true,
-        setupFiles: './setup-test.ts',
+        setupFiles: ['./setup-test.ts'],
+        env:  process.env
     },
 })
